206f5b776db725af31edf51b667c7ca05e370d22
Jake Miller <jmmiller@pio.carrollu.edu>
2015-12-20 14:56:26 -0600

    Add buttons with onTouchEvent to main screen. No current functions attached to buttons.

--- MyApplication/app/src/main/java/com/personal/jmmil/mygolfbuddy/MainActivity.java
+++ MyApplication/app/src/main/java/com/personal/jmmil/mygolfbuddy/MainActivity.java
@@ -1,17 +1,88 @@
 package com.personal.jmmil.mygolfbuddy;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton imageButton1;
+    ImageButton imageButton2;
+    ImageButton imageButton3;
+    ImageButton imageButton4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton imageButton1 = (ImageButton) this.findViewById(R.id.imageButton1);
+        imageButton1.setOnClickListener(buttonListener);
+        imageButton1.setOnTouchListener(new ButtonHighlighterOnTouchListener(imageButton1));
+
+        ImageButton imageButton2 = (ImageButton) this.findViewById(R.id.imageButton2);
+        imageButton2.setOnClickListener(buttonListener);
+        imageButton2.setOnTouchListener(new ButtonHighlighterOnTouchListener(imageButton2));
+
+        ImageButton imageButton3 = (ImageButton) this.findViewById(R.id.imageButton3);
+        imageButton3.setOnClickListener(buttonListener);
+        imageButton3.setOnTouchListener(new ButtonHighlighterOnTouchListener(imageButton3));
+
+        ImageButton imageButton4 = (ImageButton) this.findViewById(R.id.imageButton4);
+        imageButton4.setOnClickListener(buttonListener);
+        imageButton4.setOnTouchListener(new ButtonHighlighterOnTouchListener(imageButton4));
     }
+
+    private View.OnClickListener buttonListener = new View.OnClickListener(){
+
+        public void onClick(View v) {
+            ImageButton btn = (ImageButton) v;
+            btn.setColorFilter(Color.argb(255, 255, 255, 255)); // White Tint
+        }
+    };
+
+    public class ButtonHighlighterOnTouchListener implements View.OnTouchListener {
+
+        final ImageButton imageButton;
+
+        public ButtonHighlighterOnTouchListener(final ImageButton imageButton) {
+            super();
+            this.imageButton = imageButton;
+        }
+
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN: {
+                    ImageButton view = (ImageButton) v;
+                    view.getBackground().setColorFilter(0x77000000, PorterDuff.Mode.SRC_ATOP);
+                    v.invalidate();
+                    break;
+                }
+                case MotionEvent.ACTION_UP:
+
+                    // Your action here on button click
+
+                case MotionEvent.ACTION_CANCEL: {
+                    ImageButton view = (ImageButton) v;
+                    view.getBackground().clearColorFilter();
+                    view.invalidate();
+                    break;
+                }
+            }
+            return true;
+        }
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
